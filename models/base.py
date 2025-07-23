795246fcec6db0b9e0f3d679cfc83236cad5585e
Nocisuki <23020100094@pop.zjgsu.edu.cn>
2025-07-23 16:45:24 +0800

    Add files via upload

---
+++ models/base.py
@@ -0,0 +1,146 @@
+from pathlib import Path
+
+import numpy as np
+import torch
+from torch import nn
+
+from utils.toolkit import accuracy, tensor2numpy
+from scipy.spatial.distance import cdist
+
+EPSILON = 1e-8
+
+class BaseLearner(object):
+    def __init__(self, args):
+        self._cur_task = -1
+        self._err_tasks = args["err"]
+        # 几个任务
+        self._increment_classes = args["increment"]
+        self._known_classes = 0
+        self._total_classes = 0
+        self._network = None
+        self._old_network = None
+        self.eval_network = None
+        self._data_memory, self._targets_memory = np.array([]), np.array([])
+        self.topk = 2
+        self.args = args
+        self.each_task = args["increment"]
+        self.seed = args["seed"]
+        self.tasks = args["tasks"]
+        self.wandb = args["wandb"]
+        self.save_dir = args["imgs_path"]
+        self.dataset_name = args["dataset"]
+        self.nums = args["nums"]
+
+        # ----
+        args["memory_per_class"] = 20
+        args["fixed_memory"] = False
+        args["generator_model_file"] = "saved_models/generator.pth"
+
+        self._generator_checkpoint_path = Path(args["generator_model_file"])
+        self._memory_size = args["memory_size"]
+        self._memory_per_class = args.get("memory_per_class", None)
+        self._fixed_memory = args.get("fixed_memory", False)
+        self._images_per_class = 256
+        self._gen_batch_size = 64
+        # self._device = "0"
+        # self._multiple_gpus = args["device"]
+        self._device = (
+            torch.device("cuda")
+            if torch.cuda.is_available()
+            else torch.device("cpu")
+        )
+
+        # refed
+        self.old_memory_size = args["old_memory_size"]
+
+
+    @property
+    def exemplar_size(self):
+        assert len(self._data_memory) == len(
+            self._targets_memory
+        ), "Exemplar size error."
+        return len(self._targets_memory)
+
+    @property
+    def samples_per_class(self):
+        if self._fixed_memory:
+            return self._memory_per_class
+        else:
+            assert self._total_classes != 0, "Total classes is 0"
+            return self._memory_size // self._total_classes
+
+    @property
+    def feature_dim(self):
+        if isinstance(self._network, nn.DataParallel):
+            return self._network.module.feature_dim
+        else:
+            return self._network.feature_dim
+
+
+    def real_build_rehearsal_memory(self):
+        pass
+    def after_task(self):
+        pass
+    def _evaluate(self, y_pred, y_true):
+        ret = {}
+        grouped = accuracy(y_pred.T[0], y_true, self._known_classes, increment=self.each_task)
+        ret["grouped"] = grouped
+        ret["top1"] = grouped["total"]
+        ret["top{}".format(self.topk)] = np.around(
+            (y_pred.T == np.tile(y_true, (self.topk, 1))).sum() * 100 / len(y_true),
+            decimals=2,
+        )
+        return ret
+
+    def _compute_accuracy(self, model, loader):
+        model.eval()
+        correct, total = 0, 0
+        for i, (_, inputs, targets) in enumerate(loader):
+            inputs = inputs.cuda()
+            with torch.no_grad():
+                outputs = model(inputs)["logits"]
+            predicts = torch.max(outputs, dim=1)[1]
+            correct += (predicts.cpu() == targets).sum()
+            total += len(targets)
+
+        return np.around(tensor2numpy(correct) * 100 / total, decimals=2)
+
+    def eval_task(self):
+        y_pred, y_true = self._eval_cnn(self.test_loader)
+        cnn_accy = self._evaluate(y_pred, y_true)
+
+        if hasattr(self, "_class_means"):
+            y_pred, y_true = self._eval_nme(self.test_loader, self._class_means)
+            nme_accy = self._evaluate(y_pred, y_true)
+        else:
+            nme_accy = None
+
+        return cnn_accy, nme_accy
+
+
+    def _eval_cnn(self, loader):
+        self._network.eval()
+        y_pred, y_true = [], []
+        for _, (_, inputs, targets) in enumerate(loader):
+            inputs = inputs.cuda()
+            with torch.no_grad():
+                outputs = self._network(inputs)["logits"]
+            predicts = torch.topk(
+                outputs, k=self.topk, dim=1, largest=True, sorted=True
+            )[
+                1
+            ]  # [bs, topk]
+            y_pred.append(predicts.cpu().numpy())
+            y_true.append(targets.cpu().numpy())
+
+        return np.concatenate(y_pred), np.concatenate(y_true)  # [N, topk]
+
+    def _eval_nme(self, loader, class_means):
+        self._network.eval()
+        vectors, y_true = self._extract_vectors(loader)
+        vectors = (vectors.T / (np.linalg.norm(vectors.T, axis=0) + EPSILON)).T
+
+        dists = cdist(class_means, vectors, "sqeuclidean")  # [nb_classes, N]
+        scores = dists.T  # [N, nb_classes], choose the one with the smallest distance
+
+        return np.argsort(scores, axis=1)[:, : self.topk], y_true  # [N, topk]
---
+++ models/generator.py
@@ -0,0 +1,170 @@
+import copy
+
+import torch
+import torch.nn.functional as F
+from torch import nn
+
+
+class Generator(nn.Module):
+    def __init__(self, nz=100, ngf=64, img_size=32, nc=3, num_classes=57):
+        super(Generator, self).__init__()
+        self.init_size = img_size // 4
+        self.embed = nn.Embedding(num_classes, nz)
+        self.z_chunk_size = nz // 2
+        self.proj_z = nn.Linear(self.z_chunk_size, ngf * 2 * self.init_size ** 2)
+
+        self.g_blocks = nn.ModuleList([
+            nn.Sequential(
+                nn.Upsample(scale_factor=2),
+                nn.Conv2d(ngf * 2, ngf * 2, 3, stride=1, padding=1, bias=False),
+                nn.BatchNorm2d(ngf * 2),
+                nn.LeakyReLU(0.2, inplace=True)
+            ),
+            nn.Sequential(
+                nn.Upsample(scale_factor=2),
+                nn.Conv2d(ngf * 2, ngf, 3, stride=1, padding=1, bias=False),
+                nn.BatchNorm2d(ngf),
+                nn.LeakyReLU(0.2, inplace=True)
+            )
+        ])
+
+        self.attn_block = SelfAttention(ngf * 2)
+
+        self.proj_o = nn.Sequential(
+            nn.Conv2d(ngf, nc, kernel_size=3, stride=1, padding=1),
+            nn.Tanh()
+        )
+
+        self.weights_init()
+
+    def forward(self, z, labels):
+        label_embedding = self.embed(labels.long())
+        label_embedding = label_embedding.unsqueeze(-1).unsqueeze(-1)
+
+        zs = torch.split(z, self.z_chunk_size, dim=1)
+        z = zs[0]
+        # print(z.shape)
+        ys = [torch.cat([label_embedding, z_chunk.unsqueeze(-1).unsqueeze(-1)], dim=1) for z_chunk in zs[1:]]
+
+        h = self.proj_z(z)
+        h = h.view(h.size(0), -1, self.init_size, self.init_size)
+
+        for idx, g_block in enumerate(self.g_blocks):
+            h = g_block(h)
+            # print(h.shape)
+            if idx < len(ys):
+                conv_layer = nn.Conv2d(384, 128, kernel_size=1).cuda()
+                h = h + conv_layer(ys[idx])
+            if idx == 0:
+                h = self.attn_block(h)
+
+        img = self.proj_o(h)
+        return img
+
+    def weights_init(self):
+        for m in self.modules():
+            if isinstance(m, (nn.Conv2d, nn.Linear)):
+                nn.init.orthogonal_(m.weight)
+            elif isinstance(m, nn.Embedding):
+                nn.init.normal_(m.weight, 0, 0.02)
+
+    def copy(self):
+        clone = copy.deepcopy(self)
+        return clone.cuda()
+
+    def print_param(self):
+        params = 0
+        for module in self.modules():
+            params += sum([p.data.nelement() for p in module.parameters()])
+
+        print(
+            "Generator's trainable parameters:  {:.0f}M {:.0f}K {:d}".format(
+                params // 1e6, (params // 1e3) % 1000, params % 1000
+            )
+        )
+
+        param_size = 0
+        for param in self.parameters():
+            param_size += param.nelement() * param.element_size()
+        buffer_size = 0
+        for buffer in self.buffers():
+            buffer_size += buffer.nelement() * buffer.element_size()
+
+        size_all_mb = (param_size + buffer_size) / 1024 ** 2
+        print("Model Generator's size: {:.3f}MB".format(size_all_mb))
+
+
+class SelfAttention(nn.Module):
+
+    def __init__(self, in_dim):
+        super(SelfAttention, self).__init__()
+        self.query = nn.Conv2d(in_dim, in_dim // 8, kernel_size=1)
+        self.key = nn.Conv2d(in_dim, in_dim // 8, kernel_size=1)
+        self.value = nn.Conv2d(in_dim, in_dim, kernel_size=1)
+        self.gamma = nn.Parameter(torch.zeros(1))
+
+    def forward(self, x):
+        batch_size, C, width, height = x.size()
+        query = self.query(x).view(batch_size, -1, width * height).permute(0, 2, 1)  # B x N x C
+        key = self.key(x).view(batch_size, -1, width * height)  # B x C x N
+        attention = torch.bmm(query, key)  # B x N x N
+        attention = F.softmax(attention, dim=-1)
+
+        value = self.value(x).view(batch_size, -1, width * height)  # B x C x N
+        out = torch.bmm(value, attention.permute(0, 2, 1))  # B x C x N
+        out = out.view(batch_size, C, width, height)
+        out = self.gamma * out + x
+        return out
+
+# class AttentionBlock(nn.Module):
+#     """
+#     AttentionBlock Class
+#     Values:
+#     channels: number of channels in input
+#     """
+#
+#     def __init__(self, channels):
+#         super().__init__()
+#
+#         self.channels = channels
+#
+#         self.theta = nn.utils.spectral_norm(
+#             nn.Conv2d(channels, channels // 8, kernel_size=1, padding=0, bias=False)
+#         )
+#         self.phi = nn.utils.spectral_norm(
+#             nn.Conv2d(channels, channels // 8, kernel_size=1, padding=0, bias=False)
+#         )
+#         self.g = nn.utils.spectral_norm(
+#             nn.Conv2d(channels, channels // 2, kernel_size=1, padding=0, bias=False)
+#         )
+#         self.o = nn.utils.spectral_norm(
+#             nn.Conv2d(channels // 2, channels, kernel_size=1, padding=0, bias=False)
+#         )
+#
+#         self.gamma = nn.Parameter(torch.tensor(0.0), requires_grad=True)
+#
+#     def forward(self, x):
+#         spatial_size = x.shape[2] * x.shape[3]
+#
+#         # Apply convolutions to get query (theta), key (phi), and value (g) transforms
+#         theta = self.theta(x)
+#         phi = F.max_pool2d(self.phi(x), kernel_size=2)
+#         g = F.max_pool2d(self.g(x), kernel_size=2)
+#
+#         # Reshape spatial size for self-attention
+#         theta = theta.view(-1, self.channels // 8, spatial_size)
+#         phi = phi.view(-1, self.channels // 8, spatial_size // 4)
+#         g = g.view(-1, self.channels // 2, spatial_size // 4)
+#
+#         # Compute dot product attention with query (theta) and key (phi) matrices
+#         beta = F.softmax(torch.bmm(theta.transpose(1, 2), phi), dim=-1)
+#
+#         # Compute scaled dot product attention with value (g) and attention (beta) matrices
+#         o = self.o(
+#             torch.bmm(g, beta.transpose(1, 2)).view(
+#                 -1, self.channels // 2, x.shape[2], x.shape[3]
+#             )
+#         )
+#
+#         # Apply gain and residual
+#         return self.gamma * o + x
